e20f35803596eae90ea59d56572dca5652da43c1
Karthik2924 <54316824+Karthik2924@users.noreply.github.com>
2024-05-09 17:16:45 -0400

    Extended for deeplab segmentation

---
+++ src/models/deeplab.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .utils import init_param, make_batchnorm, loss_fn
+from config import cfg
+
+#model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_resnet50', pretrained=True)
+# or any of these variants
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_resnet101', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_mobilenet_v3_large', pretrained=False)
+
+
+
+class Deeplab_model(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+        self.model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_mobilenet_v3_large', pretrained=False)
+
+    def f(self, x):
+        return self.model(x)['out']
+    def forward(self, input):
+        output = {}
+        output['target'] = self.f(input['data'])
+        if 'loss_mode' in input:
+            if input['loss_mode'] == 'sup':
+                output['loss'] = loss_fn(output['target'], input['target'])
+            elif input['loss_mode'] == 'fix':
+                aug_output = self.f(input['aug'])
+                output['loss'] = loss_fn(aug_output, input['target'].detach())
+            elif input['loss_mode'] == 'fix-mix':
+                aug_output = self.f(input['aug'])
+                output['loss'] = loss_fn(aug_output, input['target'].detach())
+                mix_output = self.f(input['mix_data'])
+                output['loss'] += input['lam'] * loss_fn(mix_output, input['mix_target'][:, 0].detach()) + (
+                        1 - input['lam']) * loss_fn(mix_output, input['mix_target'][:, 1].detach())
+        else:
+            if not torch.any(input['target'] == -1):
+                output['loss'] = loss_fn(output['target'], input['target'])
+        return output
+    
+
+def deeplab(momentum=None, track=False):
+    # data_shape = cfg['data_shape']
+    # target_size = cfg['target_size']
+    # hidden_size = cfg['resnet18']['hidden_size']
+    #model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_mobilenet_v3_large', pretrained=False)
+    model = Deeplab_model()
+    model.apply(init_param)
+    model.apply(lambda m: make_batchnorm(m, momentum=momentum, track_running_stats=track))
+    return model
